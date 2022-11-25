4c72eaa154f87f26cb8ced0b46253aa7c1dbd147
barthosdac <116598222+barthosdac@users.noreply.github.com>
2022-11-25 08:57:33 +0100

    update prédicat premier étape

--- run.pl
+++ run.pl
@@ -1,16 +1,32 @@
 premiere_etape(Tbox, Abi, Abr) :-
     load_files('part1.pl'),
+    load_files('helper.pl'),
+    load_files('T-A_Box.pl'),
+
+    setof((CA, CG), equiv(CA, CG), Tboxt),       % Récupération de la TBoxt
+    setof((I1, I2), inst(I1, I2), Abit),         % Récupération de Abit
+    setof((I1, I2, R), instR(I1, I2, R), Abr),   % Récupération de Abrt
 
     % Vérification de la Tbox
     write('Vérification de la TBox ...'), nl,
-    verif_Tbox(Tbox),
+    verif_Tbox(Tboxt),
     write('Vérification de la TBox réussi'), nl,
     
     % Vérification de la Abox
     write('Vérification de la ABox ...'), nl,
-    verif_Abox([Abi | Abr]),
-    write('Vérification de la ABox réussi'), nl.
+    verif_Abox([Abit | Abr]),
+    write('Vérification de la ABox réussi'), nl,
 
+    % Vérification des autoréférencements
+    setof(X,cnamena(X),Lcc), setof(Y,cnamea(Y),Lca),
+    (verif_Autoref(Lcc, Lca)->
+    	write('Il ny a pas auto-referencement dans la TBox');
+    	write('Il y a auto-referencement dans la TBox')),nl,
+    	
+    write('Transformation des box en developpement les concepts complexes puis nnf...'), nl,
+    transforme(Abit,Abi),transforme(Tboxt,Tbox),
+    write('Transformation términée').
+    
 deuxieme_etape(Abi,Abi1,Tbox) :-
     load_files('part2.pl'),
     saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
@@ -21,14 +37,7 @@ programe :-
     % load_test_files([]),
     % run_tests,
     
-    % Import des Box
-    load_files('T-A_Box.pl'),
-    setof((CA, CG), equiv(CA, CG), Tbox),       % Récupération de la TBox
-    setof((I1, I2), inst(I1, I2), Abi),         % Récupération de Abi
-    setof((I1, I2, R), instR(I1, I2, R), Abr),   % Récupération de Abr
     premiere_etape(Tbox, Abi, Abr),             % Call de la première partie
-    
-    
     % deuxieme_etape(Abi,Abi1,Tbox),
     % troisieme_etape(Abi1,Abr),
     
