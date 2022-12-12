adfc7241bfc55184adf7ab6fd683b27a5d1696ba
barthosdac <116598222+barthosdac@users.noreply.github.com>
2022-12-12 17:20:02 +0100

    correction de l'union

--- part3.pl
+++ part3.pl
@@ -22,23 +22,37 @@ tri_Abox([A|L], Lie, Lpt, Li, Lu, [A|Ls]) :-
  */
 evolue((I, some(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
     member((I, some(R,C)), Lie).
-evolue((I, some(R,C)), Lie, Lpt, Li, Lu, Ls, [(I, some(R,C)) | Lie], Lpt, Li, Lu, Ls).
+evolue((I, some(R,C)), Lie, Lpt, Li, Lu, Ls, [(I, some(R,C)) | Lie], Lpt, Li, Lu, Ls):-
+    \+ member((I, some(R,C)), Lie).
 
 evolue((I, all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
     member((I, all(R,C)), Lpt).
-evolue((I, all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, [(I, all(R,C)) | Lpt], Li, Lu, Ls).
+evolue((I, all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, [(I, all(R,C)) | Lpt], Li, Lu, Ls):-
+    \+ member((I, all(R,C)), Lpt).
 
 evolue((I, and(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
     member((I, and(C1,C2)), Li).
-evolue((I, and(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, [(I, and(C1,C2)) | Li], Lu, Ls).
+evolue((I, and(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, [(I, and(C1,C2)) | Li], Lu, Ls) :-
+    \+ member((I, and(C1,C2)), Li).
 
 evolue((I, or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
     member((I, or(C1, C2)), Lu).
-evolue((I, or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls).
+evolue((I, or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls) :-
+    \+ member((I, or(C1, C2)), Lu).
 
-evolue(A, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
-    member(A, Ls).
-evolue(A, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [A|Ls]).
+evolue((I,C), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
+    cnamea(C),
+    member((I,C), Ls).
+evolue((I,C), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [(I,C)|Ls]) :-
+    cnamea(C),
+    \+ member((I,C), Ls).
+    
+evolue((I,not(C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls) :-
+    cnamea(C),
+    member((I,not(C)), Ls).
+evolue((I,not(C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [(I,not(C))|Ls]) :-
+    cnamea(C),
+    \+ member((I,not(C)), Ls).
 
 evolue_rec([], Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls).
 evolue_rec([A|L], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :-
@@ -75,9 +89,9 @@ resolution([], Lpt, [], Lu, Ls, Abr) :-
 	
 resolution([], [], [], Lu, Ls, Abr):-
 	non_clash(Ls),
-	transformation_and([], [], [], Lu, Ls, Abr).
+	transformation_or([], [], [], Lu, Ls, Abr).
 	
-resolution([], [], [], [], Ls, Abr):-
+resolution([], [], [], [], Ls, _):-
 	non_clash(Ls).
 
 /* 
@@ -87,30 +101,30 @@ resolution([], [], [], [], Ls, Abr):-
  */
 affiche_Ls([]).
 affiche_concept(some(R, C)) :-
-	write(' \u2203 '),
+	write('\u2203 '),
 	write(R),
 	write('.'),
 	affiche_concept(C).
 affiche_concept(all(R, C)) :-
-	write(' \u2200 '),
+	write('\u2200 '),
 	write(R),
 	write('.'),
 	affiche_concept(C),!.
 affiche_concept(and(C1, C2)) :-
 	affiche_concept(C1),
-	write(' \u2A05 '),
+	write('\u2A05 '),
 	affiche_concept(C2).
 affiche_concept(or(C1, C2)) :-
 	affiche_concept(C1),!,
-	write(' \u2A06 '),
+	write('\u2A06 '),
 	affiche_concept(C2),!.
 affiche_concept(not(C)) :-
-	write(' \u00AC '),
+	write('\u00AC '),
 	affiche_concept(C),!.
 affiche_concept(anything) :-
-	write(' \u22A4 ').
+	write('\u22A4 ').
 affiche_concept(nothing) :-
-	write(' \u22A5 ').
+	write('\u22A5 ').
 affiche_concept(C) :-
 	write(C).
 	
@@ -141,6 +155,9 @@ affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu
 	affiche_Abi(Li2),!,
 	affiche_Abi(Lu2),!,
 	affiche_Abr(Abr2),!,
+	nl,
+	(non_clash(Ls2)->write('Pas de clash');
+		write('Clash')),nl,
 	nl, write('============================='), nl, nl.
 
 /* 
@@ -150,20 +167,21 @@ affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu
  */
 transformation_or(Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls, Abr) :- 
 	% Suppression & extraction de la règle devenus inutile par la décomposition en paramètre
-	
+	write('Utilisation de la règle \u2A06 sur : '),affiche_Abi([(I, or(C1,C2))]),nl,
+	write('Br1 : avec '),affiche_concept(C1),nl,
 	% _________________________________
 	% Premier split Br1 & Nouveau noeud
-	evolue((I, C1), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1), 
+	evolue((I, C1), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
 	
 	% Print du split
 	affiche_evolution_Abox(Ls, Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr),
-	
-	% Test Clash
-	non_clash(Ls1),
 
 	% Appel récursif
-	resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr),
-
+	resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr).
+	
+transformation_or(Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls, Abr) :- 
+	write('Utilisation de la règle \u2A06 sur : '),affiche_Abi([(I, or(C1,C2))]),nl,
+	write('Br2 : avec '),affiche_concept(C2),nl,
 	% __________________________________
 	% Deuxième split Br2 & Nouveau noeud
 	evolue((I, C2),Lie, Lpt, Li, Lu, Ls, Lie2, Lpt2, Li2, Lu2, Ls2),
@@ -180,6 +198,7 @@ transformation_or(Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls, Abr) :-
   └──────────────────────────────────────────────────────────────────────────┘
  */
 transformation_and(Lie, Lpt, [(I, and(C1,C2)) | Li], Lu, Ls, Abr) :- 
+	write('Utilisation de la règle \u2A05 sur : '),affiche_Abi([(I, and(C1,C2))]),nl,
 	% Suppression & extraction de la règle devenus inutile par la décomposition en paramètre
 
 	% Nouveau noeud
@@ -194,32 +213,34 @@ transformation_and(Lie, Lpt, [(I, and(C1,C2)) | Li], Lu, Ls, Abr) :-
 	
 /* 
   ┌──────────────────────────────────────────────────────────────────────────┐
-  │ % Il existe : ∀                                                          │
+  │ % Il existe : ∃                                                          │
   └──────────────────────────────────────────────────────────────────────────┘
  */
 complete_some([(I1,some(R,C)) | Lie], Lpt, Li, Lu, Ls, Abr) :-
+	write('Utilisation de la règle \u2203 sur : '),affiche_Abi([(I1, some(R,C))]),nl,
 	% Nouveau noeud
 	genere(I2),
-	evolue((I2, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1), !, 
+	evolue((I2, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1), 
 	
 	% Print du noeud
-	affiche_evolution_Abox([(I1,some(R,C)) | Lie], Lpt, Li, Lu, Ls, Abr, Lie1, Lpt1, Li1, Lu1, Ls1, [(I1, I2, R) | Abr]),
+	affiche_evolution_Abox(Ls, [(I1,some(R,C)) | Lie], Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, [(I1, I2, R) | Abr]),
 	
 	% Appel récursif
 	resolution(Lie1, Lpt1, Li1, Lu1, Ls1, [(I1, I2, R) | Abr]).
 
 /* 
   ┌──────────────────────────────────────────────────────────────────────────┐
-  │ % Pour tout : ∃                                                          │
+  │ % Pour tout :  ∀                                                         │
   └──────────────────────────────────────────────────────────────────────────┘
  */
 deduction_all(Lie, [(I1, all(R, C)) | Lpt], Li, Lu, Ls, Abr) :-
+	write('Utilisation de la règle \u2200 sur : '),affiche_Abi([(I1, all(R,C))]),nl,
 	% Nouveau noeud
 	setof((I2, C),  member((I1, I2, R), Abr),  LC2), 
-	evolue_rec(LC2, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1), !, 
+	evolue_rec(LC2, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
 	
 	% Print du noeud
-	affiche_evolution_Abox(Lie, [(I1, all(R, C)) | Lpt], Li, Lu, Ls, Abr, Lie1, Lpt1, Li1, Lu1, Ls1, Abr), 
+	affiche_evolution_Abox(Ls, Lie, [(I1, all(R, C)) | Lpt], Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr), 
 	
 	% Appel récursif
 	resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr).
