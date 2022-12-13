96c8c94b1afa1e02ebb5d6864ecfae277b9af41a
barthosdac <116598222+barthosdac@users.noreply.github.com>
2022-12-13 09:55:08 +0100

    predicats cut enlevés

--- part3.pl
+++ part3.pl
@@ -5,16 +5,19 @@
  */
 tri_Abox([], [], [], [], [], []).
 tri_Abox([(I, some(R,C)) | L], [(I, some(R,C)) | Lie], Lpt, Li, Lu, Ls) :-
-    tri_Abox(L, Lie, Lpt, Li, Lu, Ls), !.
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
 tri_Abox([(I, all(R,C)) | L], Lie, [(I, all(R,C)) | Lpt], Li, Lu, Ls) :-
-    tri_Abox(L, Lie, Lpt, Li, Lu, Ls), !.
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
 tri_Abox([(I, and(C1,C2)) | L], Lie, Lpt, [(I, and(C1,C2)) | Li], Lu, Ls) :-
-    tri_Abox(L, Lie, Lpt, Li, Lu, Ls), !.
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
 tri_Abox([(I, or(C1,C2)) | L], Lie, Lpt, Li, [(I, or(C1,C2)) | Lu], Ls) :-
-    tri_Abox(L, Lie, Lpt, Li, Lu, Ls), !.
-tri_Abox([A|L], Lie, Lpt, Li, Lu, [A|Ls]) :-
-    tri_Abox(L, Lie, Lpt, Li, Lu, Ls), !.
-	
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
+tri_Abox([(I,C)|L], Lie, Lpt, Li, Lu, [(I,C)|Ls]) :-
+    cnamea(C),
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
+tri_Abox([(I,not(C))|L], Lie, Lpt, Li, Lu, [(I,not(C))|Ls]) :-
+    cnamea(C),
+    tri_Abox(L, Lie, Lpt, Li, Lu, Ls).
 /* 
   ┌──────────────────────────────────────────────────────────────────────────┐
   │ Evolue : intègre le premier paramètre dans la liste lui correspondant    │
@@ -101,60 +104,57 @@ resolution([], [], [], [], Ls, _):-
  */
 affiche_Ls([]).
 affiche_concept(some(R, C)) :-
-	write('\u2203 '),
+	write(' \u2203 '),
 	write(R),
 	write('.'),
 	affiche_concept(C).
 affiche_concept(all(R, C)) :-
-	write('\u2200 '),
+	write(' \u2200 '),
 	write(R),
 	write('.'),
-	affiche_concept(C),!.
+	affiche_concept(C).
 affiche_concept(and(C1, C2)) :-
 	affiche_concept(C1),
-	write('\u2A05 '),
+	write(' \u2A05 '),
 	affiche_concept(C2).
 affiche_concept(or(C1, C2)) :-
-	affiche_concept(C1),!,
-	write('\u2A06 '),
-	affiche_concept(C2),!.
+	affiche_concept(C1),
+	write(' \u2A06 '),
+	affiche_concept(C2).
 affiche_concept(not(C)) :-
-	write('\u00AC '),
-	affiche_concept(C),!.
-affiche_concept(anything) :-
-	write('\u22A4 ').
-affiche_concept(nothing) :-
-	write('\u22A5 ').
+	write(' \u00AC '),
+	affiche_concept(C).
 affiche_concept(C) :-
+	cnamea(C),
 	write(C).
 	
 affiche_Abi([]).
 affiche_Abi([(I, C) | L]):-
 	write(I), write(' : '), affiche_concept(C),nl,
-	affiche_Abi(L),!.
+	affiche_Abi(L).
 
 affiche_Abr([]).
 affiche_Abr([(I1, I2, R) | L]) :-
 	write('<'), write(I1), write(', '), write(I2), write('> : '),
 	write(R),nl,
-	affiche_Abr(L),!.
+	affiche_Abr(L).
 
 affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2):-
 	write("\033[4;30mEtat de départ :\033[0m"),nl,nl,
-	affiche_Abi(Ls1),!,
-	affiche_Abi(Lie1),!,
-	affiche_Abi(Lpt1),!,
-	affiche_Abi(Li1),!,
-	affiche_Abi(Lu1),!,
-	affiche_Abr(Abr1),!,
+	affiche_Abi(Ls1),
+	affiche_Abi(Lie1),
+	affiche_Abi(Lpt1),
+	affiche_Abi(Li1),
+	affiche_Abi(Lu1),
+	affiche_Abr(Abr1),
 	nl,
-	write("\033[4;30mEtat d'arrivée :\033[0m"),nl,nl,!,
-	affiche_Abi(Ls2),!,
-	affiche_Abi(Lie2),!,
-	affiche_Abi(Lpt2),!,
-	affiche_Abi(Li2),!,
-	affiche_Abi(Lu2),!,
-	affiche_Abr(Abr2),!,
+	write("\033[4;30mEtat d'arrivée :\033[0m"),nl,nl,
+	affiche_Abi(Ls2),
+	affiche_Abi(Lie2),
+	affiche_Abi(Lpt2),
+	affiche_Abi(Li2),
+	affiche_Abi(Lu2),
+	affiche_Abr(Abr2),
 	nl,
 	(non_clash(Ls2)->write('Pas de clash');
 		write('Clash')),nl,
